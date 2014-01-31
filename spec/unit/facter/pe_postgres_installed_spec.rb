7087335cbb0badb91912f188a3f5fb1caee5645d
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-01-30 17:15:28 -0800

    Test coverage for pe_postgres_installed "complete"

--- spec/unit/facter/pe_postgres_installed_spec.rb
+++ spec/unit/facter/pe_postgres_installed_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 require 'facter'
+require 'facter/pe_postgres_installed.rb'
 
 describe 'pe_postgres_installed' do
-#  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-  Facter::Util::Resolution.stubs(:exec).with('rpm -qa pe-postgresql-server').returns('')
+  rpmquery = 'rpm -qa pe-postgresql-server'
+  Facter.fact(:kernel).stubs(:value).returns('Linux')
 
-  it 'should return nil if rpmempty is an empty string' do
+  it 'should return nil if rpmempty is true' do
+    Facter::Util::Resolution.stubs(:exec).with(rpmquery).returns('')
     expect Facter.value('pe_postgres_installed').should be_nil
+  end
+
+  it 'should return true if rpmempty is false' do
+    Facter::Util::Resolution.stubs(:exec).with(rpmquery).returns('pe-postgresql-server')
+    expect Facter.value('pe_postgres_installed').should be_true
   end
 end
