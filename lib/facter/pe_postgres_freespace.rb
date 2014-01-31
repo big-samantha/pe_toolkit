b6c6c1724b6403ad9e7d04ea3318788290f51821
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-01-30 17:43:04 -0800

    pe_postgres_freespace fact now in bytes
    Easier to work with the fact without the G label on the end.

--- lib/facter/pe_postgres_freespace.rb
+++ lib/facter/pe_postgres_freespace.rb
@@ -1,11 +1,11 @@
-# pe_postgres_freespace.rb
+# pe_postgres_freespace_gb.rb
 # Returns the amount of free space available to Puppet Labs-vendored PostgreSQL server.
 
-Facter.add("pe_postgres_freespace") do
+Facter.add("pe_postgres_freespace_gb") do
   confine :kernel => "Linux"
   confine :pe_postgres_installed => true
 
   setcode do
-    Facter::Util::Resolution.exec("df /opt/puppet/var/lib/pgsql/ -h | awk '{ print $3 }' | tail -n 1")
+    Facter::Util::Resolution.exec("df /opt/puppet/var/lib/pgsql/ | awk '{ print $3 }' | tail -n 1")
   end
 end
