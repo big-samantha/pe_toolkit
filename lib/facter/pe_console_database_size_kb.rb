2f5a0fc8078ca6bc773fcd0a1cdce059d0df0ef7
Zachary Alex Stern <zachary@puppetlabs.com>
2014-02-06 16:19:07 -0800

    Added pe_console_database_size_kb fact.

---
+++ lib/facter/pe_console_database_size_kb.rb
@@ -0,0 +1,14 @@
+# pe_console_database_size.rb
+# Determines size of console database, in kilobytes
+
+Facter.add("pe_console_database_size_kb") do
+  confine :has_working_sudo => true
+  confine :pe_postgres_installed => true
+
+  setcode do
+    size_command = "sudo -u pe-postgres /opt/puppet/bin/psql -c \"SELECT pg_database_size(\'console\')\""
+    db_size_in_bytes = Facter::Util::Resolution.exec(size_command).to_i
+    db_size_in_kilobytes = db_size_in_bytes / 1024
+    db_size_in_kilobytes
+  end
+end
